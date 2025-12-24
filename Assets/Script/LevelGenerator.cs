38101d4b24df3b69124041c704842ab1b9a7dbe8
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-12-24 09:49:22 +0700

    checking

--- Assets/Script/LevelGenerator.cs
+++ Assets/Script/LevelGenerator.cs
@@ -171,9 +171,3 @@ public class LevelGenerator : MonoBehaviour
         }
     }
 }
-public static class GameSettings
-{
-    public static Difficulty selectedDifficulty = Difficulty.Easy;
-    public static string selectedLevelName = "easy"; // default, overwritten by menu selection
-}
-
---
+++ Assets/script Ui/GameSettings.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class GameSettings
+{
+    public static Difficulty selectedDifficulty = Difficulty.Easy;
+    public static string selectedLevelName = "easy"; // default, overwritten by menu selection
+}
